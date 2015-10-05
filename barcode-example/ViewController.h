b938d9a15cd5fa637d7426d0d18d0f099627583d
Anton Minin <vedmak17@gmail.com>
2015-10-05 20:05:57 +0300

    Initial commit

---
+++ barcode-example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  barcode-example
+//
+//  Created by Anton Minin on 05.10.15.
+//  Copyright © 2015 Anton Minin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
